73fb02d5221036fa3853321c8ff25f92d79d8258
mdopi2024 <you@example.comprogrammeropi2024@gmail.com>
2026-07-20 15:34:12 +0600

    updated package json

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,2 @@
 import {app} from './app'
 export default app;
-
-import { loadEnv } from "./env";
-
-console.log("DATABASE_URL:", !!loadEnv.DATABASE_URL);
-console.log("APP_URL:", loadEnv.APP_URL);
-console.log("BETTER_AUTH_URL:", loadEnv.BETTER_AUTH_URL);
-console.log("PORT:", loadEnv.PORT);
